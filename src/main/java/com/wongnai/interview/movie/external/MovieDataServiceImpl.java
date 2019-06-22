5e2b86595bd76cae37e01e115a54b81a74cf67d0
kennattapon <kennattapon@gmail.com>
2019-06-22 14:41:10 +0700

    First commit

--- src/main/java/com/wongnai/interview/movie/external/MovieDataServiceImpl.java
+++ src/main/java/com/wongnai/interview/movie/external/MovieDataServiceImpl.java
@@ -3,17 +3,8 @@ package com.wongnai.interview.movie.external;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 import org.springframework.web.client.RestOperations;
-import org.springframework.http.MediaType;
-import org.springframework.http.converter.HttpMessageConverter;
-import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
-import org.springframework.stereotype.Component;
+
 import com.fasterxml.jackson.databind.ObjectMapper;
-
-import org.springframework.web.client.RestTemplate;
-
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.List;
 
 @Component
 public class MovieDataServiceImpl implements MovieDataService {
@@ -28,14 +19,10 @@ public class MovieDataServiceImpl implements MovieDataService {
 
 	@Override
 	public MoviesResponse fetchAll() {
-		RestTemplate restTemplate = new RestTemplate();
-        List<HttpMessageConverter<?>> messageConverters = new ArrayList<>();
-        MappingJackson2HttpMessageConverter converter = new MappingJackson2HttpMessageConverter();
-        converter.setSupportedMediaTypes(Collections.singletonList(MediaType.ALL));
-        messageConverters.add(converter);
-        restTemplate.setMessageConverters(messageConverters);
-
-        return restTemplate.getForObject(MOVIE_DATA_URL, MoviesResponse.class);
-
+		//TODO:
+		// Step 1 => Implement this method to download data from MOVIE_DATA_URL and fix any error you may found.
+		// Please noted that you must only read data remotely and only from given source,
+		// do not download and use local file or put the file anywhere else.
+		return null;
 	}
 }
